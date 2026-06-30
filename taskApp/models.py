f88e7586415d12318c94cee0ba085b0d681f75b3
Sharjeel Ashraf <sa062385@gmail.com>
2026-06-30 22:47:57 +0500

    Files of models.py altered(taskApp)

--- taskApp/models.py
+++ taskApp/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = "Categories"
+        unique_together=['user', 'catName']
         
 
 class Task(models.Model):
